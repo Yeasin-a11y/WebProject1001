151cac45ceac6f4247d9e0a49a14ec3d2a9e8946
raghibboss <yeasinarafat19082@gmail.com>
2018-01-02 08:40:23 -0500

    added new project

---
+++ src/project15.java
@@ -0,0 +1,19 @@
+
+public class project15 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 boolean x, y; 
+		 x = true; y = false;
+		 System.out.println("x= " +x);
+		 System.out.println("y= " +y);
+
+		 int x1, y1; boolean z1;
+		 x1 = 15; y1 = 10;
+		 System.out.println("x1>y1 is " +(x1>y1));
+		 z1 = y1>x1;
+		 System.out.println("y1>x1 is " +z1);
+
+	}
+
+}
