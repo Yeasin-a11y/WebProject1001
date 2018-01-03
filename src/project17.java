d7f544196dfe3c9a27a16ba2c87695ff8e209d8f
raghibboss <yeasinarafat19082@gmail.com>
2018-01-03 11:18:29 -0500

    added project17

---
+++ src/project17.java
@@ -0,0 +1,13 @@
+
+public class project17 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10, b=15;
+		int Result;
+		Result = (a>b)?a:b;
+		System.out.println("Maximum of " +a + " and " +b + " is: " +Result);
+
+	}
+
+}
